0460ef06199cc5ee6d1426c7f146003d3081af8f
jpatrickldg <jpatrickldg@gmail.com>
2024-04-09 17:27:11 +0800

    adjust route

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Main />,
     children: [
       {
-        path: "/",
+        path: "/patrick-and-joan/",
         element: <Root />,
       },
       {
